c08dc399def1f258f0aab91749340fa253e82beb
hojin30310 <80864817+hojin30310@users.noreply.github.com>
2021-03-18 10:13:21 +0900

    .

---
+++ Assets/RouletteCountroller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+[DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+public class RouletteCountroller : MonoBehaviour
+{
+    float rotSpeed = 0.0f;
+
+    private static object GetDebuggerDisplay()
+    {
+        throw new NotImplementedException();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            this.rotSpeed = 8.0f;}
+        transform.Rotate(0, 0, rotSpeed);
+        if (Input.GetMouseButtonDown(1))
+        {
+          this.rotSpeed *= 0.987f;
+        }
+        transform.Rotate(0, 0, rotSpeed);
+    }
+}
